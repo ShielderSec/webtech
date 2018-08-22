f2d337f97ce58a4f09756673d1a9484a1ebe373c
thez3ro <io@thezero.org>
2018-08-22 10:52:32 +0200

    rebased the header parsing

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # webtech
 Identify technologies used on websites
+
+## TODO
+
+- add a main.py script with argument parsing (OptParse)
+- add Burp request parsing instead of url
+- add random user-agent argument
+- add custom user-agent argument
+- review and fix the database download
+- add an argument for a custom database file
+- make a setup.py script
--- src/webtech.py
+++ src/webtech.py
@@ -3,6 +3,7 @@ import requests
 from bs4 import BeautifulSoup
 import json
 import re
+from collections import namedtuple
 
 # TODO: change to a local import
 import database
@@ -11,6 +12,8 @@ import database
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
+Tech = namedtuple('Tech', ['name', 'version'])
+
 
 def parse_header_string(string):
     """
@@ -18,7 +21,7 @@ def parse_header_string(string):
 
     strings follow the below format:
     <string>[\\;version:\\\d][\\;confidence:\d]
-    
+
     "string" is a mandatory regex string followed by 0 or more parameters (key:value), can be empty
     parameters are divided by a \\; sequence (a backslash followed by a semicolon)
 
@@ -39,7 +42,11 @@ def parse_header_string(string):
 class WebTech():
     VERSION = 0.1
     USER_AGENT = "webtech/{}".format(VERSION)
-    COMMON_HEADERS = ['Vary', 'Connection', 'Content-Type', 'Link', 'Content-Length', 'Date', 'Content-Encoding', 'Set-Cookie', 'Cookie']
+    COMMON_HEADERS = ['Vary', 'Connection', 'Content-Type', 'Link', 'Content-Length', 'Date', 'Content-Encoding',
+                      'Set-Cookie', 'Last-Modified', 'Transfer-Encoding', 'Cache-Control', 'Strict-Transport-Security',
+                      'Expect-CT', 'X-Content-Type-Options', 'Feature-Policy', 'Referrer-Policy', 'X-Frame-Options',
+                      'X-XSS-Protection']
+    COMMON_HEADERS = [ch.lower() for ch in COMMON_HEADERS]
 
     # 'cats' tech categories
     # 'implies' website is using also this tech
@@ -51,9 +58,9 @@ class WebTech():
     # 'html' check this regex in html
     # 'meta' check this patter in meta
     # 'js' check this expression in javascript context
-    # 'cookies' check this patter in cookies 
+    # 'cookies' check this patter in cookies
     # 'script' check this pattern in scripts src
-    # 'url' check this patter in url 
+    # 'url' check this patter in url
 
     def __init__(self):
         with open(database.DATABASE_FILE) as f:
@@ -65,23 +72,38 @@ class WebTech():
         """
         self.header = {'User-Agent': self.USER_AGENT}
 
-        self.data = {}
-        self.data['url'] = url
+        # self.data contains the data fetched from the request
+        self.data = {
+            'url': None,
+            'html': None,
+            'headers': None,
+            'cookies': None,
+            'meta': None,
+            'script': None
+        }
 
-        self.scrape_url()
+        # self.report contains the information about the technologies detected
+        self.report = {
+            'tech': set(),
+            'headers': [],
+        }
 
+        self.scrape_url(url)
         self.whitelist_data()
-
         self.check_headers()
+        self.print_report()
 
-    def scrape_url(self):
+    def scrape_url(self, url):
         """
         Scrape the target URL and collects all the data that will be filtered afterwards
         """
         # By default we don't verify SSL certificates, we are only performing some useless GETs
-        response = requests.get(self.data['url'], headers=self.header, verify=False)
+        response = requests.get(url, headers=self.header, verify=False)
         print("status: {}".format(response.status_code))
 
+        # TODO: switch-case for various response.status_code
+
+        self.data['url'] = url
         self.data['html'] = response.text
         self.data['headers'] = response.headers
         self.data['cookies'] = response.cookies
@@ -101,30 +123,56 @@ class WebTech():
         """
         for header in self.data['headers']:
             # This is pretty stupid and need an alternative approach
-            if header not in self.COMMON_HEADERS:
-                print("Interesting header: {}: {}".format(header, self.data['headers'][header]))
+            if header.lower() not in self.COMMON_HEADERS:
+                self.report['headers'].append(header)
 
-        # Cookie and Set-Cookie need a special treatment
+        # Set-Cookie needs a special treatment
 
     def check_headers(self):
         """
         Check if request headers match some database headers
         """
+        # Cycle through all the db entries
         for tech in self.db["apps"]:
+            # Get the entry headers
             headers = self.db["apps"][tech].get("headers")
             if not headers:
                 continue
+            # For every tech header check if there is a match in our target
             for header in headers:
-                content = self.data['headers'].get(header)
-                if not content:
+                try:
+                    # _store, hacky way to get the original key from a request.structures.CaseInsensitiveDict
+                    real_header, content = self.data['headers']._store[header.lower()]
+                except KeyError:
+                    # tech header not found, go ahead
                     continue
+                # Parse the matching regex
                 attr, extra = parse_header_string(headers[header])
                 matches = re.search(attr, content, re.IGNORECASE)
-                if matches is not None:
-                    print("Matched tech: {}".format(matches.group(0)))
+                # Attr is empty for a "generic" tech header
+                if attr is '' or matches is not None:
+                    matched_tech = Tech(name=tech, version=None)
+                    # Remove the matched header from the Custom Header list
+                    try:
+                        self.report['headers'].remove(real_header)
+                    except ValueError:
+                        pass
+                    # The version extra data is present
                     if extra and extra['version']:
                         if matches.group(1):
-                            print("Matched tech version: {}".format(matches.group(1)))
+                            matched_tech = matched_tech._replace(version=matches.group(1))
+                    self.report['tech'].add(matched_tech)
+
+    def print_report(self):
+        print("Target URL: {}".format(self.data['url']))
+        print("Detected technologies:")
+        for tech in self.report['tech']:
+            print("\t- {} {}".format(tech.name, '' if tech.version is None else tech.version))
+
+        if self.report['headers']:
+            print("Detected the following interesting custom headers:")
+            for header in self.report['headers']:
+                print("\t- {}: {}".format(header, self.data['headers'].get(header)))
 
 
 wt = WebTech()
